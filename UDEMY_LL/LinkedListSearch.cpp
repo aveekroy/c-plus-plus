2852131812d784ba78bb5a3404463a494a8e81be
Aveek Roy <aveek.roy@outlook.com>
2022-04-20 19:50:16 -0700

    added the code for Linear Seach and improved search by move to head method

--- UDEMY_LL/LinkedListSearch.cpp
+++ UDEMY_LL/LinkedListSearch.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 using namespace std;
 
+#define endline cout << endl;
+
 struct Node
 {
     int data;
     struct Node *next;
-} *first = NULL;
+};
 
-void create(int A[], int n)
+Node *head = NULL;
+
+void Display(Node *p)
 {
-    int i;
+    while (p != NULL)
+    {
+        cout << p->data << "  ";
+        p = p->next;
+    }
+}
+
+void create(int A[], int length)
+{
     Node *t, *last;
-    last = new Node;
-    first = new Node;
-    first->data = A[0];
-    first->next = NULL;
-    last = first;
-    for (i = 1; i < n; i++)
+    head = new Node;
+    head->data = A[0];
+    head->next = NULL;
+    last = head;
+    for (int i = 1; i < length; i++)
     {
         t = new Node;
         t->data = A[i];
@@ -26,22 +37,41 @@ void create(int A[], int n)
     }
 }
 
-Node *LSearch(Node *p, int key)
+bool LinearSearch(Node *p, int key)
 {
-    Node *q = new Node;
+
+    while (p)
+    {
+        if (key == p->data)
+        {
+            cout << "Value is present in the linked list" << endl;
+            return true;
+        }
+        p = p->next;
+    }
+    cout << "Value is not present in the linked list" << endl;
+    return false;
+}
+
+// Improving Search by bringing in the key at HEAD
+bool ImproveSearch(Node *p, int key)
+{
+    Node *q = NULL;
     while (p != NULL)
     {
         if (key == p->data)
         {
             q->next = p->next;
-            p->next = first;
-            first = p;
-            return p;
+            p->next = head;
+            head = p;
+            cout << "Value is present in the linked list" << endl;
+            return true;
         }
         q = p;
         p = p->next;
     }
-    return NULL;
+    cout << "Value is not present in the linked list" << endl;
+    return false;
 }
 
 Node *RSearch(Node *p, int key)
@@ -55,10 +85,15 @@ Node *RSearch(Node *p, int key)
 
 int main()
 {
-    Node *temp;
     int A[] = {3, 5, 7, 10, 25, 8, 32, 2};
-    create(A, 8);
-    temp = LSearch(first, 22);
-    cout << temp->data;
+    create(A, sizeof(A) / sizeof(int));
+    cout << "Linked List: " << endl;
+    Display(head);
+    endline;
+    cout << "Searching...: " << LinearSearch(head, 25) << endl;
+    endline;
+    cout << "Searching...: " << ImproveSearch(head, 2) << endl;
+    Display(head);
+
     return 0;
 }
