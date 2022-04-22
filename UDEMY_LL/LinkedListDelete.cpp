b17fbbbb5d18a3ddf15b62c15c512d52fbbc27a6
Aveek Roy <aveek.roy@outlook.com>
2022-04-21 20:11:23 -0700

    deleting a linked list

---
+++ UDEMY_LL/LinkedListDelete.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+Node *head = NULL;
+
+// Creating a linked list from an array
+void create(int A[], int length)
+{
+    Node *t, *last;
+    head = new Node;
+    head->data = A[0];
+    head->next = NULL;
+    last = head;
+    for (int i = 1; i < length; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+}
+
+int count(Node *p)
+{
+    int count = 0;
+    while (p != NULL)
+    {
+        count++;
+        p = p->next;
+    }
+    return count;
+}
+
+void Delete(struct Node *p, int index)
+{
+
+    Node *d = head;
+    Node *q;
+    if (index < 0 || index > count(p))
+        return;
+
+    // When a node needs to be deleted at the beginning
+    if (index == 0)
+    {
+        head = head->next;
+        delete d;
+    }
+    // When a node needs to be inserted in between two nodes or at the last
+    else
+    {
+        for (int i = 0; i < index - 1; i++)
+        {
+            q = p;
+            p = p->next;
+        }
+        q->next = p->next;
+        delete p;
+    }
+}
+
+int main()
+{
+
+    int A[] = {10, 20, 30, 40, 50};
+    create(A, 5);
+    Display(head);
+    cout << endl;
+
+    Delete(head, 0);
+    Display(head);
+    cout << endl;
+
+    Delete(head, 0);
+    Display(head);
+    cout << endl;
+
+    Delete(head, 2);
+    Display(head);
+    cout << endl;
+
+    return 0;
+}
