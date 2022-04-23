8d10abd0e41340ddaaf93fa1abaa53dd7b3e35e5
Aveek Roy <aveek.roy@outlook.com>
2022-04-23 08:18:05 -0700

    added the isLoop check for a LL

---
+++ LINKEDLIST/10_LinkedListLoop.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+Node *head = NULL;
+
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    head = (struct Node *)malloc(sizeof(struct Node));
+    head->data = A[0];
+    head->next = NULL;
+    last = head;
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+int isLoop(struct Node *f)
+{
+    struct Node *p, *q;
+    p = q = f;
+    do
+    {
+        p = p->next;
+        q = q->next;
+        q = q ? q->next : q;
+    } while (p && q && p != q);
+
+    return (p == q) ? 1 : 0;
+}
+
+int main()
+{
+    struct Node *t1, *t2;
+    int A[] = {10, 20, 30, 40, 50};
+    create(A, 5);
+    t1 = head->next->next;
+    t2 = head->next->next->next->next;
+    t2->next = t1;
+    cout << isLoop(head);
+    return 0;
+}
